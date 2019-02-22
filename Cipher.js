aa24cb47eb304437406baf989dfce65261974616
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-22 17:46:34 +0100

    Update Cipher.js

--- Cipher.js
+++ Cipher.js
@@ -1,6 +1,22 @@
 function rot13(str) { // LBH QVQ VG!
+  //Create an array for every letter of the ABC
+  const abc=["A", "B", "C", "D", "E", "F", "G", "H", "I",
+             "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+             "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  //create an array of the input characters
+  const inputArr=str.split("");
+  //create a var for the answer string
+  let answer="";
   
-  return str;
+  //Walk through the inputArr
+  //If the element does not equal a letter, concatenate into the answer unchanged
+  //If the element is a letter move it 13 positions to the right in the abc array
+  //and return that element
+  //If it would run over the array, step from the beginning with however many steps it would overrun by
+  //Then add the letter to the answer string
+  
+  //return the answer
+  return answer;
 }
 
 // Change the inputs below to test
